2fd62be0b2d6c079bfd84cc72e5cee38504adaa1
chengxianghe <chengxianghe@outlook.com>
2017-05-23 15:22:50 +0800

    fix bug

---
+++ XHSegmentView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHSegmentView
+//
+//  Created by chengxianghe on 2017/5/22.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
